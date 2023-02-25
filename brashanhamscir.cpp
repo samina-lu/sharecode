b499d67682cfa2ac8ed2db5644e86cc80700116c
syedasaminahussain552@gmail.com <syedasaminahussain552@gmail.com>
2023-02-25 07:26:57 +0600

    add

---
+++ brashanhamscir.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<graphics.h>
+using namespace std;
+void drawCircle(int x,int y,int xc,int yc){
+
+    putpixel(xc+x , yc+y ,RED);
+    putpixel(xc-x , yc-y ,YELLOW);
+    putpixel(xc-x , yc+y ,RED);
+    putpixel(xc+x , yc-y ,YELLOW);
+    putpixel(xc+y , yc+x ,RED);
+    putpixel(xc-y,  yc-x ,YELLOW);
+    putpixel(xc-y , yc+x ,RED);
+    putpixel(xc+y, yc-x,RED);
+
+    }
+int main()
+{
+    int gd= DETECT, gm;
+    int  xc,yc,r;
+    cout<<"Enter the x,y& r point";
+    cin>>xc>>yc>>r;
+    initgraph(&gd, &gm," ");
+     int d = 1-r ;
+    int x = 0, y =r;
+
+    while(x<=y)
+    {
+        drawCircle(x,y,xc, yc);
+        if (d<0)
+        {
+
+            d= d+2*(x)+3;
+
+        }
+        else
+        {
+
+
+        d=d+2*(x-y)+5;
+            y--;
+        }
+        x++;
+        delay(50);
+
+    }
+
+    getch();
+
+    closegraph();
+}
+
+
+
+
+
+
+
